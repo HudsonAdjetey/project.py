772365c093c478aafbfcad156ea93ba119eeb805
Emmanuel Hudson <adjetey545@gmail.com>
2024-04-04 14:15:03 -0700

    modified media team app

--- MediaTeam/main.py
+++ MediaTeam/main.py
@@ -1,6 +1,22 @@
-import customtkinter as ck
+import tkinter.ttk
 
+import customtkinter as ck
+from CTkTable import *
 from CTkSpinbox import *
+from tkinter import *
+
+def load_data():
+    name = name_entry.get()
+    status = status_combobox.get()
+    age = age_entry.get()
+    role = roll_combo.get()
+    content = [
+        [name, status, age, role]
+    ]
+    if agree_checkBtn.get():
+        for cont in content:
+            treeView.insert("", tkinter.END, values=cont)
+
 
 
 cm_values = ["Single", "Married", "Engaged", "Divorced"]
@@ -14,10 +30,18 @@ window.title("Judah Temple Media")
 window.geometry("720x450")
 
 frame_main = ck.CTkFrame(master=window,)
-frame_main.pack(pady=(20, 0))
+frame_main.pack(pady=(20, 0),)
+
 
 personal_frame = ck.CTkFrame(master=frame_main, width=600)
 personal_frame.grid(column=0, row=0, sticky="news", pady=(0, 10))
+
+# Configure column and row weights
+# frame_main.columnconfigure(0, weight=1)  # Column 0
+# frame_main.rowconfigure(0, weight=0)     # Row 0
+
+
+
 
 name_label = ck.CTkLabel(master=personal_frame, text="Name", font=ck.CTkFont(family="sans-serif", size=15, weight="bold"))
 name_label.grid(column=0, row=0, sticky="w")
@@ -29,11 +53,11 @@ age_label = ck.CTkLabel(master=personal_frame, text="Age",  font=ck.CTkFont(fami
 age_label.grid(column=2, row=0, sticky="w")
 
 name_entry = ck.CTkEntry(master=personal_frame, font=ck.CTkFont(family="sans-serif", size=17), width=200)
-name_entry.grid(column=0, row=1, padx=5)
+name_entry.grid(column=0, row=1, padx=5,)
 
 status_combobox = ck.CTkComboBox(master=personal_frame, values=cm_values, font=ck.CTkFont(family="sans-serif", size=17), width=150)
 status_combobox.current_index = 0
-status_combobox.grid(column=1, row=1, padx=5)
+status_combobox.grid(column=1, row=1, padx=5, )
 
 age_entry = CTkSpinbox(personal_frame,
           start_value = 15,
@@ -54,7 +78,8 @@ roll_combo = ck.CTkComboBox(master=info_frame, values=rm_values)
 roll_combo.currentIndex = 0
 roll_combo.grid(row=2, column=0)
 
-agree_checkBtn = ck.CTkCheckBox(master=info_frame, text="By Checking means you agree to the terms", font=ck.CTkFont(family="sans-serif", size=15, weight="bold"))
+
+agree_checkBtn = ck.CTkCheckBox(master=info_frame, text="By Checking means you agree to the terms", font=ck.CTkFont(family="sans-serif", size=15, weight="bold"), )
 agree_checkBtn.grid(row=2, column=1)
 
 # Now configure padding for widgets in info_frame
@@ -62,11 +87,51 @@ for widget in info_frame.winfo_children():
     widget.grid_configure(padx=10)
 
 
-add_btn = ck.CTkButton(master=frame_main, text="Add")
+add_btn = ck.CTkButton(master=frame_main, text="Add", command=load_data)
 add_btn.grid(row=3, column=0, columnspan=1, sticky="ew", pady=(30, 0))
 
 
 # Tree view
-tree_view =
+value = [
+        ["Name", "Age", "Status", "Patent", "Study"],
+         [1,2,3,4,5],
+         [1,2,3,4,5],
+         [1,2,3,4,5],
+         [1,2,3,4,5],
+[1,2,3,4,5],
+         [1,2,3,4,5],
+[1,2,3,4,5],
+         [1,2,3,4,5],
+[1,2,3,4,5],
+         [1,2,3,4,5],
+[1,2,3,4,5],
+         [1,2,3,4,5],
+[1,2,3,4,5],
+         [1,2,3,4,5]
+]
+
+
+
+cols = [
+    "Name",
+    "Status",
+    "Age",
+    "Role"
+]
+
+treeScroll = tkinter.Scrollbar(master=window)
+treeScroll.pack(side="right", fill="y")
+
+treeView = tkinter.ttk.Treeview(master=window, show="headings", columns=cols, )
+
+
+
+
+for col in cols:
+    treeView.heading(column=col, text=col)
+    treeView.column(col, width=180)
+
+treeView.pack()
+treeScroll.config(command=treeView.yview)
 
 window.mainloop()
